352d6a6938141bf27741251c300c8b540b80ced9
Mike Schouw <mike.schouw@gmail.com>
2023-08-11 11:50:35 +0200

    update key

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "hello_world" {
   create_package = false
   s3_existing_package = {
     bucket     = local.artifacts_bucket
-    key        = "go/bever_proxy.zip"
-    version_id = data.aws_s3_object.go_version["go/bever_proxy.zip"].version_id
+    key        = "go/hello_world.zip"
+    version_id = data.aws_s3_object.go_version["go/hello_world.zip"].version_id
   }
 }
